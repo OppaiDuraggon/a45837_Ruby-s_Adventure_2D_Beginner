9a6e51bf43352a34f78148232826f24837aecba2
Reinaldo_Teixeira <doo-kan@hotmail.com>
2023-04-17 23:43:54 +0100

    World Interactions - Damage Zones and Enemies-COMPLETE

--- Assets/Scripts/CollectibleHealth.cs
+++ Assets/Scripts/CollectibleHealth.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollectibleHealth : MonoBehaviour
+public class HealthCollectible : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
