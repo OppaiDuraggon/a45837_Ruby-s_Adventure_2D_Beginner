c12b4299c9c617b95ba4a7cbcbe83042bfde61f5
Reinaldo_Teixeira <doo-kan@hotmail.com>
2023-04-20 23:32:08 +0100

    Visual Styling - Particles-COMPLETE

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-﻿public class EnemyController : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
     public float speed;
     public bool vertical;
     public float changeTime = 3.0f;
 
+    public ParticleSystem smokeEffect;
+    
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
@@ -83,6 +85,7 @@ using UnityEngine;
         rigidbody2D.simulated = false;
         //optional if you added the fixed animation
         animator.SetTrigger("Fixed");
+        
+        smokeEffect.Stop();
     }
 }
-    
